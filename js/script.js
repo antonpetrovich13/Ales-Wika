87e0dbca96b35fb0125b9505ed34cbaffcd10595
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2021-06-07 23:51:26 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper('.history__body', {
+	autoHeight: true,
+	navigation: {
+		nextEl: '.history__next',
+		prevEl: '.history__prev',
+	},
+	speed: 800,
+	loop: true,
+	effect: "fade",
+	zoom: true,
+});
